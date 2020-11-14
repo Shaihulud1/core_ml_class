51a26aca961aba7786064e213ebc5648d89d9a85
Ilia <godofcode1@icloud.com>
2020-11-15 01:42:14 +0400

    Initial Commit

---
+++ hot dog classification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hot dog classification
+//
+//  Created by Илья Дернов on 15.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
